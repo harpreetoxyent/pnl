a71af025eb582a27489c35d49e52c6a79ef8b4e2
Harpreet Singh <harpreet_singh@oxyent.com>
2014-03-03 18:02:30 +0530

    Deleted the bin folder and added missing projects

---
+++ refs/heads/master/components/CrawlerComponent/src/com/pnl/util/Utility.java
@@ -0,0 +1,64 @@
+package com.pnl.util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class Utility {
+	
+	public static boolean addFile(String source, String dest) throws IOException {
+        Configuration conf = new Configuration();
+
+        // Conf object will read the HDFS configuration parameters from these
+        // XML files.
+        conf.addResource(new Path("/home/ruchi/hadoop-1.1.0/conf/core-site.xml"));
+        conf.addResource(new Path("/home/ruchi/hadoop-1.1.0/conf/hdfs-site.xml"));
+
+        FileSystem fileSystem = FileSystem.get(conf);
+
+        // Get the filename out of the file path
+        String filename = source.substring(source.lastIndexOf('/') + 1,
+            source.length());
+
+        // Create the destination path including the filename.
+        if (dest.charAt(dest.length() - 1) != '/') {
+            dest = dest + "/" + filename;
+        } else {
+            dest = dest + filename;
+        }
+
+        // System.out.println("Adding file to " + destination);
+
+        // Check if the file already exists
+        Path path = new Path(dest);
+        if (fileSystem.exists(path)) {
+            System.out.println("File " + dest + " already exists in hdfs.");
+            return false;
+        }
+
+        // Create a new file and write data to it.
+        FSDataOutputStream out = fileSystem.create(path);
+        InputStream in = new BufferedInputStream(new FileInputStream(
+            new File(source)));
+
+        byte[] b = new byte[1024];
+        int numBytes = 0;
+        while ((numBytes = in.read(b)) > 0) {
+            out.write(b, 0, numBytes);
+        }
+
+        // Close all the file descripters
+        in.close();
+        out.close();
+        fileSystem.close();
+        return true;
+    }
+
+}
---
+++ refs/heads/master/components/SearchComponent/src/com/pnl/indexer/IndexDriver.java
@@ -0,0 +1,72 @@
+package com.pnl.indexer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class IndexDriver extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		// TODO: Add some checks here to validate the input path
+		int exitCode = ToolRunner.run(new Configuration(), new IndexDriver(),args);
+		System.exit(exitCode);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+
+		/*
+		 * Job job = new Job(); job.setJarByClass(IndexDriver.class);
+		 * job.setJobName("IndexDriver"); job.setSpeculativeExecution(false);
+		 * 
+		 * // Set Input and Output paths FileInputFormat.setInputPaths(job, new
+		 * Path(args[0])); FileOutputFormat.setOutputPath(job, new
+		 * Path(args[1]));
+		 * 
+		 * // Use TextInputFormat
+		 * job.setInputFormatClass(TextInputFormat.class);
+		 * 
+		 * //Mapper has no output job.setMapperClass(IndexMapper.class);
+		 * 
+		 * job.setMapOutputKeyClass(NullWritable.class);
+		 * job.setMapOutputValueClass(NullWritable.class);
+		 * job.setNumReduceTasks(0);
+		 * 
+		 * boolean success;
+		 * 
+		 * success = job.waitForCompletion(true); //System.exit(success ? 0 :
+		 * 1); return success?0:1;
+		 */
+
+		JobConf conf = new JobConf(getConf(), IndexDriver.class);
+		conf.setJobName("IndexDriver");
+		conf.setSpeculativeExecution(false);
+
+		// Set Input and Output paths
+		FileInputFormat.setInputPaths(conf, new Path(args[0].toString()));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1].toString()));
+		// Use TextInputFormat
+		//conf.setInputFormat(TextInputFormat.class);
+		conf.setInputFormat(FileInputFormat.class);
+
+		// Mapper has no output
+		conf.setMapperClass(IndexMapper.class);
+		conf.setMapOutputKeyClass(NullWritable.class);
+		conf.setMapOutputValueClass(NullWritable.class);
+		conf.setNumReduceTasks(0);
+		JobClient.runJob(conf);
+		return 0;
+	}
+}
