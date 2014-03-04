beb7fe49c38634d5878f1b86da1a0b3e23d71229
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-04 18:07:59 +0530

    Added IComponent and project dependencies in SocialMedia

--- refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/SocialMedia.java
+++ refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/SocialMedia.java
@@ -1,6 +1,16 @@
 package com.pnl.component.socialmedia;
 
-public class SocialMedia implements ISocialMedia {
+import java.util.Hashtable;
+
+import org.dom4j.Document;
+
+import com.oxymedical.component.baseComponent.IComponent;
+import com.oxymedical.component.baseComponent.exception.ComponentException;
+import com.oxymedical.component.baseComponent.maintenance.annotations.MaintenancePublisher;
+import com.oxymedical.core.commonData.IHICData;
+import com.oxymedical.core.maintenanceData.IMaintenanceData;
+
+public class SocialMedia implements ISocialMedia , IComponent{
 
 	/**
 	 * @param args
@@ -8,6 +18,49 @@ public class SocialMedia implements ISocialMedia {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Hashtable<String, Document> configData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IHICData getHicData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setHicData(IHICData hicData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@MaintenancePublisher
+	public void maintenance(IMaintenanceData maintenanceData) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
