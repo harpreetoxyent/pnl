3f56fdb394838e5a6efc88088bdfac7c297bb2a8
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-04 17:27:58 +0530

    Changing Package Structure in SocialMedia

---
+++ refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/ISocialMedia.java
@@ -0,0 +1,5 @@
+package com.pnl.component.socialmedia;
+
+public interface ISocialMedia {
+
+}
---
+++ refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/SocialMedia.java
@@ -0,0 +1,13 @@
+package com.pnl.component.socialmedia;
+
+public class SocialMedia implements ISocialMedia {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
