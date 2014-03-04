dea4ab99b8db66b0cdb31188fe6bb74a18e1f847
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-04 17:58:29 +0530

    Added more packages to SocialMedia

---
+++ refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/utilities/ActionConstants.java
@@ -0,0 +1,10 @@
+package com.pnl.component.socialmedia.utilities;
+
+public interface ActionConstants 
+{
+	String REQUEST_TYPE = "requestType";
+	String REGISTRATION = "registration";
+	String IMPORT_CONTACTS = "importContacts";
+	String SHARE = "share";
+
+}
