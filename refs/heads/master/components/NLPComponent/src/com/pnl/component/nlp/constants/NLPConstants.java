98b2c4bcbaa619962291556af3fc086d0b1ac9eb
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-05 10:35:30 +0530

    Added constants file to NLP package

---
+++ refs/heads/master/components/NLPComponent/src/com/pnl/component/nlp/constants/NLPConstants.java
@@ -0,0 +1,13 @@
+package com.pnl.component.nlp.constants;
+
+public class NLPConstants {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
