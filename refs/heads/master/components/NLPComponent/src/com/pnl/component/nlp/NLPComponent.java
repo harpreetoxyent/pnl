9e9e45cd85f8d8aecc7be93e7bf5a9657a8c2946
Harpreet Singh <harpreet_singh@oxyent.com>
2014-03-05 18:01:26 +0530

    NLP Component Fixed

--- refs/heads/master/components/NLPComponent/src/com/pnl/component/nlp/NLPComponent.java
+++ refs/heads/master/components/NLPComponent/src/com/pnl/component/nlp/NLPComponent.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Hashtable;
 
 import org.dom4j.Document;
@@ -33,195 +35,173 @@ import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
 
-public class NLPComponent implements INLPComponent , IComponent{
+public class NLPComponent implements INLPComponent, IComponent {
 
-	public static void main(String[] args) throws IOException 
-	{
+	public static void main(String[] args) throws IOException {
 		InputStream sentenceModelFile = null;
 		InputStream tokenModelFile = null;
 		InputStream nameModelFile = null;
 		InputStream locationModelFile = null;
-		InputStream posModelFile = null;
-		
+		URI posModelFile = null;
+
 		SentenceModel sentModel = null;
 		TokenizerModel tokenModel = null;
 		TokenNameFinderModel nameFinderModel = null;
 		POSModel posModel = null;
 		TokenNameFinderModel locationFinderModel = null;
-		
-//		sentenceModelFile = new FileInputStream("/PNL/Technology/NLP_Models/en-sent.bin");
-		sentenceModelFile = NLPComponent.class.getResourceAsStream("model/en-sent.bin");
-
-		tokenModelFile = NLPComponent.class.getResourceAsStream("model/en-token.bin");
-		nameModelFile = NLPComponent.class.getResourceAsStream("model/NLP_Models/en-ner-person.bin");
-		locationModelFile = NLPComponent.class.getResourceAsStream("model/en-ner-location.bin");
-		posModelFile = NLPComponent.class.getResourceAsStream("model/en-pos-maxent.bin");
-		
-		String paragraph  = "This isn't the greatest example sentence in the world because I've seen better. Ankit Kumar Singh sweet and Mike Smith Kumar lives in California";
+		String paragraph = "This isn't the greatest example sentence in the world because I've seen better. Ankit Kumar Singh sweet and Mike Smith Kumar lives in California";
 		String line;
 		try 
 		{
-			
-			sentModel = new SentenceModel(sentenceModelFile);  
+			// sentenceModelFile = new
+			// FileInputStream("/PNL/Technology/NLP_Models/en-sent.bin");
+			sentenceModelFile = NLPComponent.class
+					.getResourceAsStream("/com/pnl/component/nlp/model/en-sent.bin");
+			tokenModelFile = NLPComponent.class
+					.getResourceAsStream("/com/pnl/component/nlp/model/en-token.bin");
+			nameModelFile = NLPComponent.class
+					.getResourceAsStream("/com/pnl/component/nlp/model/en-ner-person.bin");
+			locationModelFile = NLPComponent.class
+					.getResourceAsStream("/com/pnl/component/nlp/model/en-ner-location.bin");
+			posModelFile = NLPComponent.class.getResource(
+					"/com/pnl/component/nlp/model/en-pos-maxent.bin").toURI();
+
+
+
+			sentModel = new SentenceModel(sentenceModelFile);
 			tokenModel = new TokenizerModel(tokenModelFile);
 			nameFinderModel = new TokenNameFinderModel(nameModelFile);
 			locationFinderModel = new TokenNameFinderModel(locationModelFile);
-//			posModel = new POSModelLoader().loadModel(posModelFile);
-		} 
-		
-		catch (FileNotFoundException e1)
-		{
+			posModel = new POSModelLoader().load(new File(posModelFile));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		catch (IOException e) 
-		{
-	          e.printStackTrace();
-	    }
-		 
-        finally 
-        {
-          if (sentenceModelFile != null)
-          {
-            try 
-            {
-            	sentenceModelFile.close();
-            }
-            catch (IOException e) 
-            {
-            }
-          }
-          if (tokenModelFile != null)
-          {
- 	           try 
- 	           {
- 	        	   tokenModelFile.close();
- 	           }
- 	           catch (IOException e) 
- 	           {
- 	           }
-          }
-          if (nameModelFile != null)
-          {
- 	           try 
- 	           {
- 	        	  nameModelFile.close();
- 	           }
- 	           catch (IOException e) 
- 	           {
- 	           }
-          }
-         
-        }
-	       
-		
-		//Sentence Model
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			if (sentenceModelFile != null) {
+				try {
+					sentenceModelFile.close();
+				} catch (IOException e) {
+				}
+			}
+			if (tokenModelFile != null) {
+				try {
+					tokenModelFile.close();
+				} catch (IOException e) {
+				}
+			}
+			if (nameModelFile != null) {
+				try {
+					nameModelFile.close();
+				} catch (IOException e) {
+				}
+			}
+
+		}
+
+		// Sentence Model
 		SentenceDetectorME sdetector = new SentenceDetectorME(sentModel);
 		String sentences[] = sdetector.sentDetect(paragraph);
-		System.out.println("Output of sentence model");	
-		
+		System.out.println("Output of sentence model");
+
 		System.out.println(sentences[0]);
-		System.out.println(sentences[1]);	
-		
-		System.out.println("*****************");	
-		
+		System.out.println(sentences[1]);
+
+		System.out.println("*****************");
+
 		// Tokenizer Model
 		Tokenizer tokenizer = new TokenizerME(tokenModel);
-		String tokens[] = tokenizer.tokenize(paragraph);	 
-		System.out.println("Output of token model");	
+		String tokens[] = tokenizer.tokenize(paragraph);
+		System.out.println("Output of token model");
 
-		for (String a : tokens)
-		{
+		for (String a : tokens) {
 			System.out.println(a);
-		}	
+		}
 		System.out.println("*****************");
-		
-		 
-		// Name Finder Model	
-		 NameFinderME nameFinder  = new NameFinderME(nameFinderModel);
-		 Span nameSpans[] = nameFinder.find(tokens);
-		 System.out.println("Output of name finder model");	 
-			for(Span s: nameSpans)
-			{
-				System.out.println(s.toString());
-			}	
-			System.out.println("*****************");
-			
-			if (tokenModelFile != null)
-	         {
-		           try 
-		           {
-		        	   tokenModelFile.close();
-		           }
-		           catch (IOException e) 
-		           {
-		           }
-	         }
-			
-			// Location Finder Model	
-			 NameFinderME locationFinder  = new NameFinderME(locationFinderModel);
-			 Span locationSpans[] = locationFinder.find(tokens);
-			 System.out.println("Output of location finder model");	 
-				for(Span s: locationSpans)
-				{
-					System.out.println(s.toString());
-				}	
-				System.out.println("*****************");
-				
-				if (tokenModelFile != null)
-		         {
-			           try 
-			           {
-			        	   tokenModelFile.close();
-			           }
-			           catch (IOException e) 
-			           {
-			           }
-		         }
-		// Part-Of-Speech Model	
-		
+
+		// Name Finder Model
+		NameFinderME nameFinder = new NameFinderME(nameFinderModel);
+		Span nameSpans[] = nameFinder.find(tokens);
+		System.out.println("Output of name finder model");
+		for (Span s : nameSpans) {
+			System.out.println(s.toString());
+		}
+		System.out.println("*****************");
+
+		if (tokenModelFile != null) {
+			try {
+				tokenModelFile.close();
+			} catch (IOException e) {
+			}
+		}
+
+		// Location Finder Model
+		NameFinderME locationFinder = new NameFinderME(locationFinderModel);
+		Span locationSpans[] = locationFinder.find(tokens);
+		System.out.println("Output of location finder model");
+		for (Span s : locationSpans) {
+			System.out.println(s.toString());
+		}
+		System.out.println("*****************");
+
+		if (tokenModelFile != null) {
+			try {
+				tokenModelFile.close();
+			} catch (IOException e) {
+			}
+		}
+		// Part-Of-Speech Model
+
 		PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
 		POSTaggerME tagger = new POSTaggerME(posModel);
-		ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(paragraph));
+		ObjectStream<String> lineStream = new PlainTextByLineStream(
+				new StringReader(paragraph));
 		perfMon.start();
-		System.out.println("Output of pos model");	
-		while ((line = lineStream.read()) != null) 
-		{
-			 
-			String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
+		System.out.println("Output of pos model");
+		while ((line = lineStream.read()) != null) {
+
+			String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE
+					.tokenize(line);
 			String[] tags = tagger.tag(whitespaceTokenizerLine);
-	 
+
 			POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
 			System.out.println(sample.toString());
-	 
+
 			perfMon.incrementCounter();
 		}
 		perfMon.stopAndPrintFinalResult();
 		System.out.println("*****************");
-				
+
 	}
 
 	@Override
 	public void start(Hashtable<String, Document> configData) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void run() throws ComponentException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void stop() throws ComponentException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void destroy() throws ComponentException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -233,15 +213,14 @@ public class NLPComponent implements INLPComponent , IComponent{
 	@Override
 	public void setHicData(IHICData hicData) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	@MaintenancePublisher
 	public void maintenance(IMaintenanceData maintenanceData) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
