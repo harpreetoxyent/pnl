92a554ba48b89f32c6c2446ccabf8205cf6d4221
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-05 10:37:16 +0530

    Added package and class for exception

---
+++ refs/heads/master/components/NLPComponent/src/com/pnl/component/nlp/exception/NLPException.java
@@ -0,0 +1,13 @@
+package com.pnl.component.nlp.exception;
+
+public class NLPException {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
