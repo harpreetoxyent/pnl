1b3703d02b831cae6271d38240544f9056371d83
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-14 17:12:33 +0530

    Added successUrl

--- refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/SocialMediaComponent.java
+++ refs/heads/master/components/SocialMediaComponent/META-INF/maven/org.brickred/socialauthdemo/src/com/pnl/component/socialmedia/SocialMediaComponent.java
@@ -9,10 +9,12 @@ import org.brickred.socialauth.AuthProvider;
 import org.brickred.socialauth.Contact;
 import org.brickred.socialauth.Profile;
 import org.dom4j.Document;
+
 import com.oxymedical.component.baseComponent.IComponent;
 import com.oxymedical.component.baseComponent.annotations.EventSubscriber;
 import com.oxymedical.component.baseComponent.exception.ComponentException;
 import com.oxymedical.component.baseComponent.maintenance.annotations.MaintenancePublisher;
+import com.oxymedical.core.commonData.HICData;
 import com.oxymedical.core.commonData.IData;
 import com.oxymedical.core.commonData.IHICData;
 import com.oxymedical.core.maintenanceData.IMaintenanceData;
@@ -72,14 +74,13 @@ public class SocialMediaComponent implements ISocialMediaComponent, IComponent {
 		socialAuthManager.setSocialAuthConfig(configAuth);
 
 		// URL of YOUR application which will be called after authentication
-		String successUrl = "http://opensource.brickred.com/socialauthdemo/socialAuthSuccessAction.do";
+		String successUrl = "http://example.com:8080/HICFrameworkServlet/form.zul";
 
 		// Now get selected provider by user from zul
 		IData data = hicData.getData();
 		String providerId = (String) data.getFormPattern().getFormValues()
 				.get("providerId");
-
-		String callbackURL = "http://";
+		System.out.println("**** Provider ID is ****" + providerId);
 		// get Provider URL to which you should redirect for authentication.
 		// id can have values "facebook", "twitter", "yahoo" etc. or the OpenID
 		// URL
@@ -87,13 +88,29 @@ public class SocialMediaComponent implements ISocialMediaComponent, IComponent {
 				successUrl);
 		AuthProvider provider = socialAuthManager
 				.connect(new HashMap<String, String>());
+		System.out.println("**** Provider  is ****" + provider.getProviderId());
+
 		// get profile
 		Profile p = provider.getUserProfile();
 		// you can obtain profile information
 		System.out.println(p.getFirstName());
-
+//
 		// OR also obtain list of contacts
 		List<Contact> contactsList = provider.getContactList();
 		return hicData;
+	}
+	public static void main(String[] args)
+	{
+		SocialMediaComponent socialComponent = new SocialMediaComponent();
+		HICData hicData = new HICData();
+		hicData.getData().getFormPattern().getFormValues().put("providerId", "facebook");
+		try {
+			socialComponent.execute(hicData);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+				
 	}
 }
