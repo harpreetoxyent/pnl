2cb3d003d714cafce41714b99dfe1fa47df3a1d0
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-23 14:10:37 +0530

    Integrating Rules for PNL Use Case

---
+++ refs/heads/master/config/PNLAnswerEngine/rules/erls/SocialMediaRules.erl
@@ -0,0 +1,54 @@
+<ruleset name="SocialMediaRecommendation">
+	<variable-list>
+		<variable>
+			<type>com.oxymedical.core.commonData.HICData</type>
+			<id>dataObject</id>
+		</variable>
+		<variable>
+			<type>com.recommender.datamodel.ISocialMediaObject</type>
+			<id>socObject</id>
+		</variable>
+	</variable-list>
+	<rule name="TestRule1">
+		<salience> 0 </salience>
+		<if>
+			<condition-list>
+				<condition name="cond1">
+					(dataObject.getLocation() == "US")
+				</condition>
+				<condition name="cond2">
+					((dataObject.getAge() >= 18) && (dataObject.getAge() <= 25))
+				</condition>
+			</condition-list>
+		</if>
+		<then>
+			<consequence-list>
+				<consequence name="c1">
+					socObject.addUniURLIntoData()
+				</consequence>
+			</consequence-list>
+			</then>
+		<url> "http://www.google.com" </url>
+	</rule>
+	<rule name="TestRule2">
+		<salience> 0 </salience>
+		<if>
+			<condition-list>
+				<condition name="cond1">
+					(dataObject.getLocation() == "US")
+				</condition>
+				<condition name="cond2">
+					((dataObject.getAge() >= 25) && (dataObject.getAge() <= 40))
+				</condition>
+			</condition-list>
+		</if>
+			<then>
+				<consequence-list>
+					<consequence name="c1">
+						socObject.addLocURLIntoData()
+					</consequence>
+				</consequence-list>
+			</then>
+			<url> "http://www.google.com" </url>
+	</rule>
+</ruleset>
