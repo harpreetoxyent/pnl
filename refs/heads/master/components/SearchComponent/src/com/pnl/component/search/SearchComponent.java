6b29dacb3d0752c4b3a2ec82f293d93f564fea02
ravneetoxyent <ravneet_kaur@oxyent.com>
2014-03-24 17:10:00 +0530

    Modified Search Component

--- refs/heads/master/components/SearchComponent/src/com/pnl/component/search/SearchComponent.java
+++ refs/heads/master/components/SearchComponent/src/com/pnl/component/search/SearchComponent.java
@@ -1,7 +1,58 @@
 package com.pnl.component.search;
 
-public class SearchComponent implements ISearchComponent
-{	
+import java.util.Hashtable;
+
+import org.dom4j.Document;
+
+import com.oxymedical.component.baseComponent.IComponent;
+import com.oxymedical.component.baseComponent.exception.ComponentException;
+import com.oxymedical.core.commonData.IHICData;
+import com.oxymedical.core.maintenanceData.IMaintenanceData;
+
+public class SearchComponent implements ISearchComponent, IComponent
+{
+
+	@Override
+	public void start(Hashtable<String, Document> configData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() throws ComponentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IHICData getHicData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setHicData(IHICData hicData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maintenance(IMaintenanceData maintenanceData) {
+		// TODO Auto-generated method stub
+		
+	}	
 	
 
 }
